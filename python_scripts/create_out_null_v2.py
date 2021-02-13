23dc4c51fe2de28a22c6d71317d273d76cd39122
Matti Brandmaier <33371968+MattiBRNDMR@users.noreply.github.com>
2021-02-13 21:54:03 +0100

    Create create_out_null_v2.py
    V2 of the "create out null" script.
    - removes a lot of unnecessary code

---
+++ python_scripts/create_out_null_v2.py
@@ -0,0 +1,29 @@
+import hou
+
+def create_out_nodes():
+   
+    #get nodes
+    nodes = hou.selectedNodes()
+    prefix = "OUT_"
+    
+    for node in nodes:
+        
+        #query each selected node and ask for it's out null name
+        null_name = hou.ui.readInput("Name for OUT-Null after:" + " " + str(node), buttons=("Create", "Cancel"))
+        
+        #if "create" was clicked
+        if null_name[0] == 0:
+            
+            #create null node, add underscores and capitalize the name, add black color to out null
+            out_null = node.createOutputNode("null", prefix + null_name[1].replace(" ", "_").upper())
+            out_null.setColor(hou.Color((0,0,0)))
+    
+        
+        #if "cancel" was clicked    
+        else: 
+            
+            exit()
+        
+        
+        
+create_out_nodes()
